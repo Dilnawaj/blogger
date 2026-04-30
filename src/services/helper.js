0c9f7eec2db12e358bd21c954d03a60b4222718b
Dilnawaj Khan <48027940+Dilnawaj@users.noreply.github.com>
2026-04-30 14:04:44 +0530

    updated base file too

--- src/services/helper.js
+++ src/services/helper.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { doLogout, getToken } from "../auth/Index";
 
-export const BASE_URL = "http://56.228.22.53:8081/";
+export const BASE_URL = "http://16.171.172.94:8081/";
 
 export const myAxios = axios.create({
   baseURL: BASE_URL,
