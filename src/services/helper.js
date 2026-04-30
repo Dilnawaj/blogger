e537f9e07a49e98415e42478f054bf95f79a2004
Dilnawaj Khan <48027940+Dilnawaj@users.noreply.github.com>
2026-04-30 16:26:59 +0530

    static ip

--- src/services/helper.js
+++ src/services/helper.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { doLogout, getToken } from "../auth/Index";
 
-export const BASE_URL = "/api/"; //"http://16.171.172.94:8081/";
+export const BASE_URL = "/api/";
 
 export const myAxios = axios.create({
   baseURL: BASE_URL,
